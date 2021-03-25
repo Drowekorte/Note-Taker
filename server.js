9673658973c7e67e1aaf85416cd90a3b611a1e65
Drowekorte <drowekorte@gmail.com>
2021-03-25 12:33:22 -0700

    Added readFile and uuid

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid'); 
+
+const PORT = process.env.PORT || 8080;
 
 
 app.use(express.urlencoded({extended: true}));
@@ -15,4 +19,28 @@ app.get("/notes", function(req, res){
     res.sendFile(path.join(_dirname, "public", "notes.html"));
 })
 
-app.listen)prompt, ( => console.log("App listening on port" + PORT));
+app.get("/api/notes", function(rec, res){
+
+    fs.readFile("./db/db.json", "utf8", function(req, res){
+        res.json(JSON.parse(data))
+    })
+
+});
+
+app.post("/api/notes", function(rec, res){
+    const note = {
+        id: uuidv4();
+        title: req.body.title,
+        text: req.body.text,
+    }
+console.log(note);
+
+res.json(note);
+});
+
+app.delete("/api/notes:id", function(rec, res){
+const { id } = req.params;
+});
+
+app.listen(PORT, () => console.log("App listening on port" + PORT));
+
