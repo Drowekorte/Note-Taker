6426eca08503979b1ceb37ae31d0e172a1edb1e6
Drowekorte <drowekorte@gmail.com>
2021-03-26 10:15:42 -0700

    Trying to get the notes to post

--- server.js
+++ server.js
@@ -2,45 +2,52 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid'); 
+const { v4: uuidv4 } = require('uuid');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(_dirname, "public", "index.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 })
 
-app.get("/notes", function(req, res){
-    res.sendFile(path.join(_dirname, "public", "notes.html"));
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
 })
 
-app.get("/api/notes", function(rec, res){
+app.get("/api/notes", function (req, res) {
 
-    fs.readFile("./db/db.json", "utf8", function(req, res){
+    fs.readFile("./db/db.json", "utf8", function (req, res) {
         res.json(JSON.parse(data))
     })
 
 });
 
-app.post("/api/notes", function(rec, res){
+app.post("/api/notes", function (req, res) {
     const note = {
-        id: uuidv4();
+        id: uuidv4(),
         title: req.body.title,
         text: req.body.text,
     }
-console.log(note);
-
-res.json(note);
+    fs.readFile("./db/db.json", note, function (req, res) { 
+        res.json(note)
+    
+        fs.writeFile("./db/db.json", note, function (req, res) { 
+        req.json(JSON.stringify(data))
+        data.push(note)   
+    })  
+   })   
+   console.log(note);  
+    // res.json(note);
 });
 
-app.delete("/api/notes:id", function(rec, res){
-const { id } = req.params;
+app.delete("/api/notes:id", function (req, res) {
+    const { id } = req.params;
 });
-
+ 
+ 
 app.listen(PORT, () => console.log("App listening on port" + PORT));
-
