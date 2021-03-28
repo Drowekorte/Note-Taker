822009d6b2234d9fed1727cf49454faf63489e67
Drowekorte <drowekorte@gmail.com>
2021-03-28 10:27:46 -0700

    Finally got the delete button to work

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
+const database = path.join(__dirname, "/db/db.json");
 
 const PORT = process.env.PORT || 8000;
 
@@ -21,7 +22,7 @@ app.get("/notes", function (req, res) {
 
 app.get("/api/notes", function (req, res) {
 
-    fs.readFile("./db/db.json", "utf8", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
         res.json(JSON.parse(data))
     })
 
@@ -33,21 +34,53 @@ app.post("/api/notes", function (req, res) {
         title: req.body.title,
         text: req.body.text,
     }
-    fs.readFile("./db/db.json", note, function (req, res) { 
-        res.json(note)
-    
-        fs.writeFile("./db/db.json", note, function (req, res) { 
-        req.json(JSON.stringify(data))
-        data.push(note)   
-    })  
-   })   
-   console.log(note);  
-    // res.json(note);
+
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        var parsedData = JSON.parse(data);
+
+        parsedData.push(note);
+
+        parsedData = JSON.stringify(parsedData);
+        // console.log("Stringified parsed data with push ", parsedData)
+
+        fs.writeFile("./db/db.json", parsedData, function (err, data) {
+            if (err) throw err
+        })
+    })
+    res.redirect("back")
+})
+
+
+
+
+app.delete("/api/notes/:id", function (req, res) {
+    console.log("DELETE")
+    const pathToJson = path.join(__dirname, "/db/db.json");
+    fs.readFile(pathToJson, "utf8", function (err, data) {
+        // res.json(JSON.parse(data))
+        let parsedJson = JSON.parse(data)
+        console.log(parsedJson)
+
+        for (let i = 0; i < parsedJson.length; i++) {
+
+            if (parsedJson[i].id == req.params.id) {
+                // Splice takes i position, and then deletes the 1 note.
+                parsedJson.splice(i, 1);
+                break;
+            }
+        }
+        const stringJson =  JSON.stringify(parsedJson)
+            fs.writeFile(__dirname + '/db/db.json', stringJson, function (err, data) {
+                if (err) {
+                    return console.log(err);
+                } else {
+                    console.log("Your note was deleted!");
+                }
+            });
+        res.send("Delete")
+    })   
 });
 
-app.delete("/api/notes:id", function (req, res) {
-    const { id } = req.params;
-});
- 
- 
+
+
 app.listen(PORT, () => console.log("App listening on port" + PORT));
