2dc71f17a4cffa1df6111049e20bb7a1e3751669
Drowekorte <drowekorte@gmail.com>
2021-03-28 11:20:09 -0700

    Added all comments, created a video and working on the readme

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
 # Note-Taker
+
+### Create a note taker application.
+
+## Name
+
+A portfolio to showcase my recent work. <br>
+https://drowekorte.github.io/Portfolio/index.html
+
+<br>
+
+## Description
+
+Here's a simple to use note taker for your production team. Easily add, save, and delete notes by writing to a JSON file with Express.js.
+* HTML
+* CSS
+* JavaScript
+* Express.js
+<br>
+
+## Requirements: VSCode
+
+To clone my code for your own project:
+* Copy the git SSH to your clipboard <br>
+![Screen Shot](assets/README/github.jpg)
+* Create a local file to place the GitHub file into
+* Open your terminal, find the file you've just created
+* Type
+```
+Your Computer:~ Your File/ git clone git@github.com:Drowekorte/Portfolio.git 
+
+```
+* ...then, check that you’ve cloned all of the files with...
+
+```
+Your Computer:~ Your File/ git status
+
+```
+
+* To open in VSCode, type
+
+```
+Your Computer:~ Your File/ git pull
+
+```
+* ...and enjoy.
+
+<br>
+
+## Useage
+This is used to review my learning process
+
+<br>
+
+## Support
+For questions, feel free to contact me at through GitHub
+
+<br>
+
+## Roadmap
+Check back for updates
+
+<br>
+
+## Contributing
+No contributions at this time
+
+<br>
+
+## Authors and acknowledgment
+https://www.w3schools.com/<br>
+https://css-tricks.com/<br>
+https://developer.mozilla.org/en-US<br>
+
+<br>
+
+## License
+This is not currently licensed.
+
+<br>
+
+## Project status
+Currently, this project is in a developmental stage.
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const fs = require("fs");
+// This uses a randomly generated id for each note.
 const { v4: uuidv4 } = require('uuid');
 const database = path.join(__dirname, "/db/db.json");
 
@@ -11,15 +12,15 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
+// Here we're calling on the html page that the app will start with.
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 })
-
+// This "get" calls on the html page that the notes will be displayed on.
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
 })
-
+// This "get" calls on the json file that stores our notes in json format.
 app.get("/api/notes", function (req, res) {
 
     fs.readFile("./db/db.json", "utf8", function (err, data) {
@@ -27,54 +28,58 @@ app.get("/api/notes", function (req, res) {
     })
 
 });
-
+// This "post" will read, parse, stringify, and re-write the .json file.
 app.post("/api/notes", function (req, res) {
+    // Create a variable for the user input to be stored.
     const note = {
+        // This adds a unique id to each note
         id: uuidv4(),
         title: req.body.title,
         text: req.body.text,
     }
-
+    // Read the json file and parse the data. 
     fs.readFile("./db/db.json", "utf8", function (err, data) {
         var parsedData = JSON.parse(data);
-
+        // Push the parsed user input to the note variable.
         parsedData.push(note);
-
+        // Stringify the json data so it can be read.
         parsedData = JSON.stringify(parsedData);
         // console.log("Stringified parsed data with push ", parsedData)
-
+        // Write to the json file for saving.
         fs.writeFile("./db/db.json", parsedData, function (err, data) {
             if (err) throw err
         })
     })
+    // Return to the empty note
     res.redirect("back")
 })
 
 
 
-
+// This "delete" will read, parse, stringify, and re-write the .json file using the note id to identify the note to be deleted.
 app.delete("/api/notes/:id", function (req, res) {
     console.log("DELETE")
     const pathToJson = path.join(__dirname, "/db/db.json");
     fs.readFile(pathToJson, "utf8", function (err, data) {
-        // res.json(JSON.parse(data))
+       
         let parsedJson = JSON.parse(data)
         console.log(parsedJson)
 
         for (let i = 0; i < parsedJson.length; i++) {
-
+            // This loop will identify if the id matches 
             if (parsedJson[i].id == req.params.id) {
-                // Splice takes i position, and then deletes the 1 note.
+                // Takes the parsed json, calls the index, then removes one item.
                 parsedJson.splice(i, 1);
                 break;
             }
         }
+        // We re-write to th json file here to delete that note by id.
         const stringJson =  JSON.stringify(parsedJson)
             fs.writeFile(__dirname + '/db/db.json', stringJson, function (err, data) {
                 if (err) {
-                    return console.log(err);
+                    return alert(err);
                 } else {
-                    console.log("Your note was deleted!");
+                    alert("Your note was deleted!");
                 }
             });
         res.send("Delete")
@@ -82,5 +87,5 @@ app.delete("/api/notes/:id", function (req, res) {
 });
 
 
-
+// Console logs the PORT we're listening on.
 app.listen(PORT, () => console.log("App listening on port" + PORT));
